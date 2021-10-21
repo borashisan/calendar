b0411fa90968b5935c905f72073dbc01d094dd6b
borashisan <tomochuro731@gmail.com>
2021-10-21 18:07:30 +0900

    create basic calendar

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '2.7.1'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.1.4', '>= 6.1.4.1'
+gem 'simple_calendar', '2.4.3'
 # Use mysql as the database for Active Record
 gem 'mysql2', '~> 0.5'
 # Use Puma as the app server
---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,41 @@
+class EventsController < ApplicationController
+  def index
+    @events = Event.all
+  end
+
+  def new
+    @events = Event.new
+  end
+
+  def show
+    @events = Event.find(params[:id])
+  end
+
+  def create
+    Event.create(event_parameter)
+    redirect_to events_path
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to events_path, notice: "削除しました"
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    if @event.update(event_parameter)
+      redirect_to events_path, notice: "編集しました"
+    else
+      render 'edit'
+    end
+  end
+
+  def event_parameter
+    params.require(:event).permit(:title, :content, :start_time)
+  end
+end
