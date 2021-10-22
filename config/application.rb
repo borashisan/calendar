7ef05b5099c34c9e4940eb750fe15c0e6a1d6262
borashisan <tomochuro731@gmail.com>
2021-10-22 14:53:55 +0900

    timezone Tokyo

--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,7 @@ module Calendar
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
