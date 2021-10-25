32f435b17390aaef647ac92667bdc23492c4ef42
borashisan <tomochuro731@gmail.com>
2021-10-25 16:34:34 +0900

    edit config

--- config/application.rb
+++ config/application.rb
@@ -36,5 +36,7 @@ module Calendar
     config.generators.system_tests = nil
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+
+    config.i18n.default_locale = :ja
   end
 end
