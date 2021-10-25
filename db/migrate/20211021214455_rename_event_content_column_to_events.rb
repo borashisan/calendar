b373b427c398269f192ae0fcd8118ae33b0ad77f
borashisan <tomochuro731@gmail.com>
2021-10-25 21:32:49 +0900

    不要なテーブルとマイグレーションファイルを削除

--- db/migrate/20211021214455_rename_event_content_column_to_events.rb
+++
@@ -1,5 +0,0 @@
-class RenameEventContentColumnToEvents < ActiveRecord::Migration[6.1]
-  def change
-    rename_column :events, :event_content, :content
-  end
-end
--- db/migrate/20211021214616_rename_event_time_column_to_events.rb
+++
@@ -1,5 +0,0 @@
-class RenameEventTimeColumnToEvents < ActiveRecord::Migration[6.1]
-  def change
-    rename_column :events, :event_time, :start_time
-  end
-end
